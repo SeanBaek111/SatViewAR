56db68724325985c0c4089c54ce0dbbad1b36311
SeanBaek111 <dbaek111@gmail.com>
2023-11-06 01:26:19 +1000

    Fix orientation to enforce portrait mode

--- gnssfinder/AppDelegate.swift
+++ gnssfinder/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
 
     // MARK: UISceneSession Lifecycle
 

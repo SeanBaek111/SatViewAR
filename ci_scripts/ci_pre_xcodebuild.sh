d1aa951296b0468da02ab7a18b296e79ec81c462
SeanBaek111 <dbaek111@gmail.com>
2023-11-27 23:22:42 +1000

    fix: Update CocoaPods installation to use Bundler in Xcode Cloud
    - Modified the ci_pre_xcodebuild.sh script to use Bundler for installing CocoaPods.
    - Added checks to install Bundler if it's not present and set up a Gemfile.
    - Implemented 'bundle exec pod install' to manage CocoaPods dependencies.
    - This update resolves the Ruby version compatibility issue in Xcode Cloud and ensures CocoaPods is installed correctly.

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-# Check if CocoaPods is installed
-if ! command -v pod > /dev/null; then
-  echo "Installing CocoaPods..."
-  gem install --user-install cocoapods
+# Install Bundler if not present
+if ! command -v bundle > /dev/null; then
+  echo "Installing Bundler..."
+  gem install --user-install bundler
 fi
 
 # Add local gems to PATH
 export PATH=$PATH:$(ruby -r rubygems -e 'puts Gem.user_dir')/bin
 
+# Check if CocoaPods is installed, if not install using Bundler
+if ! command -v pod > /dev/null; then
+  echo "Installing CocoaPods via Bundler..."
+  bundle init
+  echo "gem 'cocoapods'" >> Gemfile
+  bundle install
+fi
+
 # Install pods
-pod install
+bundle exec pod install

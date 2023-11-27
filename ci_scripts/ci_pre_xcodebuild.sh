74943345cb0edfe5d8c79c261e47ba774680f533
SeanBaek111 <dbaek111@gmail.com>
2023-11-27 23:11:26 +1000

    fix: Resolve permission issue for CocoaPods installation in Xcode Cloud
    - Modified the ci_pre_xcodebuild.sh script to install CocoaPods at the user level.
    - Added '--user-install' flag to 'gem install cocoapods' to bypass permission issues.
    - Included user-level RubyGems PATH to ensure 'pod' command is found.
    - This change addresses the 'Gem::FilePermissionError' encountered in Xcode Cloud and ensures successful installation of CocoaPods.

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,8 +3,11 @@
 # Check if CocoaPods is installed
 if ! command -v pod > /dev/null; then
   echo "Installing CocoaPods..."
-  gem install cocoapods
+  gem install --user-install cocoapods
 fi
+
+# Add local gems to PATH
+export PATH=$PATH:$(ruby -r rubygems -e 'puts Gem.user_dir')/bin
 
 # Install pods
 pod install

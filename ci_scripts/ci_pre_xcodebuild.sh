a109f967334ac69ad331d9b084acc68804771b51
SeanBaek111 <dbaek111@gmail.com>
2023-11-27 22:57:00 +1000

    feat: Automate CocoaPods installation in Xcode Cloud
    - Updated ci_pre_xcodebuild.sh to check for and install CocoaPods if not present.
    - This ensures that CocoaPods dependencies are correctly installed during the Xcode Cloud build process.
    - The script now automatically installs CocoaPods using 'gem install cocoapods' command if it's not already installed.
    - This update is aimed at resolving the 'pod command not found' issue in Xcode Cloud environment and streamlining the build process.

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-#  ci_pre_xcodebuild.sh
-#  GnssFinder
-#
-#  Created by Sean on 27/11/2023.
-#  
+# Check if CocoaPods is installed
+if ! command -v pod > /dev/null; then
+  echo "Installing CocoaPods..."
+  gem install cocoapods
+fi
+
+# Install pods
 pod install
